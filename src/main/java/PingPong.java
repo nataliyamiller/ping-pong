18117ef292a738a203ba492c78da0da273ff8b01
jakekaad <kaadalac@gmail.com>
2015-08-10 14:28:05 -0700

    input all test and complete

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -6,9 +6,28 @@ public class PingPong {
 
         String[] numberList = new String[userNumber];
 
-       for (Integer index = 0; index < numberList.length; index++) {
-           numberList[index] = index + "";
+       for (Integer index = 1; index <= numberList.length; index++) {
+           if (index % 15 ==0) {
+               numberList[index - 1] = "ping-pong";
+           }
+           else if (index %3==0 ){
+              numberList[index - 1] = "ping";
+
+           }
+           else if (index %5==0){
+              numberList[index - 1] = "pong";
+
+           }
+
+           else{
+               numberList[index -1 ] = index + "";
+           }
+
+
+
+
         }
+
 
         return numberList;
    }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -4,10 +4,34 @@ import static org.junit.Assert.*;
 public class PingPongTest {
 
     @Test
-    public void isPingPong_forNumberDivisibleByThree_true() {
+    public void isPingPong_forNumberReturnArray_true() {
         PingPong pingPong = new PingPong();
-        String[] expectedArray = { "0", "1", "2" };
-        assertEquals(expectedArray, pingPong.isPingPong(3));
+        String[] expectedArray = { "1", "2" };
+        assertEquals(expectedArray, pingPong.isPingPong(2));
 
     }
+
+    @Test
+    public void isPingPong_forNumberDivisibleByThree_true() {
+    PingPong pingPong = new PingPong();
+    String[] expectedArray = { "1", "2", "ping" };
+    assertEquals(expectedArray, pingPong.isPingPong(3));
+}
+
+   @Test
+   public void isPingPong_forNumberDivisibleByFive_true(){
+   PingPong pingPong = new PingPong();
+    String[] expectedArray = {"1", "2", "ping", "4", "pong"};
+    assertEquals(expectedArray, pingPong.isPingPong(5));
+
+   }
+
+   @Test
+   public void isPingPong_forNumberDivisibleByFifteen_true() {
+       PingPong pingPong = new PingPong();
+       String[] expectedArray = {"1", "2", "ping", "4", "pong", "ping", "7", "8", "ping",
+       "pong", "11", "ping", "13", "14", "ping-pong" };
+       assertEquals(expectedArray, pingPong.isPingPong(15));
+   }
+
 }
